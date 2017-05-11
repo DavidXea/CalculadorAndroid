6c05a9d5dd88614d78e55d1581b20d68d129b071
David Maciel Pereira <5981506@univali.br>
2017-05-11 11:56:52 -0300

    first commit

---
+++ app/src/main/java/com/david/p1/projetonovo/Principal.java
@@ -0,0 +1,13 @@
+package com.david.p1.projetonovo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interface_principal);
+    }
+}
